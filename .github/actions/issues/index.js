c327a05c4399652061b900a322531f91f8fa07ca
Koteshwar Rao Myneni <koteshwar.myneni@infomagnus.com>
2023-04-19 12:11:46 +0530

    updated 22

--- .github/actions/issues/index.js
+++ .github/actions/issues/index.js
@@ -1,20 +1,23 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { async } = require("rxjs/internal/scheduler/async");
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
 
-try {
-  const token = core.getInput('token');
-  const title = core.getInput('title');
-  const body = core.getInput('body');
-  const assignees = core.getInput('assignees');
+    const octokit = new github.Github(token);
+    const response = await octokit.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split("/n") : undefined,
+    });
 
-  const octokit = new github.Github(token);
-  const response = await octokit.issues.create({
-    ...github.context.repo,
-    title,body,
-    assignees: assignees ? assignees.split('/n'): undefined
-  });
-
-  core.setOutput('issues', JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error.message);
+    core.setOutput("issues", JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
