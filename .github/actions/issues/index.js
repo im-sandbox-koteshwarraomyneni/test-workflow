99b3c30dfd684c9486fb3d612f9058050fc0a930
Koteshwar Rao Myneni <koteshwar.myneni@infomagnus.com>
2023-04-19 11:59:34 +0530

    Update index.js

--- .github/actions/issues/index.js
+++ .github/actions/issues/index.js
@@ -8,7 +8,7 @@ try {
   const assignees = core.getInput('assignees');
 
   const octokit = new github.Github(token);
-  const response = octokit.issues.create({
+  const response = await octokit.issues.create({
     ...github.context.repo,
     title,body,
     assignees: assignees ? assignees.split('/n'): undefined
